7212b76ce73f767223ae4c3d3a30cabd6eec6331
zack <yovel.zack@gmail.com>
2015-03-28 02:54:33 +0300

    added all_scanner to test.py and changed file name to string_initiate_scanner_test_case.py

--- test/string_initiate_scanner_test_case.py
+++ test/string_initiate_scanner_test_case.py
@@ -8,6 +8,87 @@ class StringInitiateScannerTestCase(unittest.TestCase):
         self._default_scanner = StringInitiateScanner()
         self._hidden_scanner = \
             StringInitiateScanner(read=False, read_hidden=True)
+        self._all_scanner = StringInitiateScanner(read_hidden=True)
+
+    def test_check_with_all_scanner_true1(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "# INIT: this string should be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_true2(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "# INIT:this string should be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_true3(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "#INIT: this string should be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_true4(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "# INIT :this string should be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_true5(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "## INIT: this string should be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_true6(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "## INIT:this string should be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_true7(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "##INIT: this string should be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_true8(self):
+        self.assertTrue(self._all_scanner.check_for_trigger(
+            "## INIT :this string should be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_false1(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "this string should not be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_false2(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "##M INIT: this string should not be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_false3(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "##^INIT: this string should not be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_false4(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "## INIT&: this string should not be identified as a hidden trigger"
+        ))
+
+    def test_check_with_all_scanner_false5(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "this string should not be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_false6(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "#M INIT: this string should not be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_false7(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "#^INIT: this string should not be identified as a trigger"
+        ))
+
+    def test_check_with_all_scanner_false4(self):
+        self.assertTrue(not self._all_scanner.check_for_trigger(
+            "# INIT&: this string should not be identified as a trigger"
+        ))
 
     def test_check_with_hidden_scanner_true1(self):
         self.assertTrue(self._hidden_scanner.check_for_trigger(
